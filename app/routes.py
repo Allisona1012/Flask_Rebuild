515cb25935359e48f0ef9c1766abf87bb06f06ee
Allisona1012 <allisona1012@gmail.com>
2022-02-04 20:34:20 -0600

    the start to templates

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from app import app 
+from flask import render_template
 
-@app.route('/')
-def index():
-    return "Hello World"
+@app.route('/') #this is part of the url 
+def index():#this is a functoin that will run when we go to said web page
+    return render_template('index.html') #this pulls the template that will show up on said webpage
 
 @app.route('/name')
 def name():
