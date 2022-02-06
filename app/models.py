7487465ae6bc6287937154b531ffa26aa994b512
Allisona1012 <allisona1012@gmail.com>
2022-02-05 20:40:33 -0600

    added a user to db

--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
 from app import db
-import datetime
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.column(db.String(50),unique=True,nullable=False)
+    username = db.Column(db.String(50),unique=True, nullable=False)
     email=db.Column(db.String(80),unique=True, nullable=False)
-    password= db.Column(db.String(256),nullable=False)
+    password= db.Column(db.String(256), nullable=False)
     date_created=db.Column(db.DateTime, nullable = False, default =datetime.utcnow)
 
     def __init__(self, **kwargs):
--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
+from pickle import FALSE
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URL=os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = FALSE
 
